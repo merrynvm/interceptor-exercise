7cd4056490b5714b7ff97562328e3a0f1c7afc95
merry <rozzimario18@gmail.com>
2025-05-06 18:30:58 +0200

    initial commit

---
+++ src/main/java/com/example/interceptor_exercise/InterceptorExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.interceptor_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/interceptor_exercise/InterceptorExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interceptor_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
