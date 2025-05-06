45e910333555ffc4a37bb607f15b97dea4d693a5
merry <rozzimario18@gmail.com>
2025-05-06 18:42:24 +0200

    created: new controller, BasicController & LegacyController

---
+++ src/main/java/com/example/interceptor_exercise/controller/BasicController.java
@@ -0,0 +1,18 @@
+package com.example.interceptor_exercise.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/time")
+public class BasicController {
+
+    @GetMapping
+    public LocalDateTime timeNow(){
+        return LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/example/interceptor_exercise/controller/LegacyController.java
@@ -0,0 +1,16 @@
+package com.example.interceptor_exercise.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/legacy")
+public class LegacyController {
+
+    @GetMapping
+    public String legacy(){
+        return "This is just old code";
+    }
+
+}
