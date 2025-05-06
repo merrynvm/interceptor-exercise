a87dbe0e527a93f13697262a2ed7b32908a5f420
merry <rozzimario18@gmail.com>
2025-05-06 18:39:21 +0200

    created: interceptor and configured webConfig

--- src/main/java/com/example/interceptor_exercise/config/WebConfig.java
+++ src/main/java/com/example/interceptor_exercise/config/WebConfig.java
@@ -1,8 +1,23 @@
 package com.example.interceptor_exercise.config;
 
+import com.example.interceptor_exercise.interceptor.APILoggingInterceptor;
+import com.example.interceptor_exercise.interceptor.LegacyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private APILoggingInterceptor apiLoggingInterceptor;
 
+    @Autowired
+    private LegacyInterceptor legacyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiLoggingInterceptor);
+        registry.addInterceptor(legacyInterceptor);
+    }
 }
