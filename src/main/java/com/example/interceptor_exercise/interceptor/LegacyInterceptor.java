3ad002d83f0073d4e76f3f71c1b459429ffb1f2c
merry <rozzimario18@gmail.com>
2025-05-12 11:19:35 +0200

    deploy configuration

--- src/main/java/com/example/interceptor_exercise/interceptor/LegacyInterceptor.java
+++ src/main/java/com/example/interceptor_exercise/interceptor/LegacyInterceptor.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class LegacyInterceptor implements HandlerInterceptor {
@@ -14,13 +13,4 @@ public class LegacyInterceptor implements HandlerInterceptor {
         return false;
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-    }
 }
